2729c7bed84444924d9ab6bb12f12364b0c5349e
David Askwith <dkaskwith@hotmail.com>
2017-03-11 13:38:06 -0500

    back button error fixed

--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -1,7 +1,43 @@
 //the main javascript page
 
+//app creation
+var app = angular.module("myApp", ["ngRoute"]);
 
-//creates back button function
-function goBack() {
-    window.history.back();
-}
+//routing for page content importing
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "pages/main.html"
+    })
+    .when("/cart", {
+        templateUrl : "pages/cart.html"
+    })
+    .when("/beers", {
+        templateUrl : "pages/beers.html"
+    })
+    .when("/mixed", {
+        templateUrl : "pages/mixed.html"
+
+    }).when("/popular", {
+        templateUrl : "pages/popular.html"
+
+    });
+});
+
+//controller used for back button
+app.controller('myCtrl', ['$scope', function($scope) {
+
+  
+}]);
+
+//directive for a back button
+app.directive("backButton", ["$window", function ($window) {
+    return {
+        restrict: "A",
+        link: function (scope, elem, attrs) {
+            elem.bind("click", function () {
+                $window.history.back();
+            });
+        }
+    };
+}]);
